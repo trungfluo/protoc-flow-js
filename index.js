dce1cd34c846c05c1ce22497c038b3078a275167
trungutt <trung.nguyen@adyoulike.com>
2017-09-09 15:32:02 +0200

    chore(index): create empty project

---
+++ index.js
@@ -0,0 +1,2 @@
+// @flow
+console.log('1 = ', JSON.stringify(1, null, 5));
---
+++ src/a.test.js
@@ -0,0 +1,13 @@
+// @flow
+
+import test from 'ava';
+
+test('foo', t => {
+  t.pass();
+});
+
+test('bar', async t => {
+  const bar = Promise.resolve('bar');
+
+  t.is(await bar, 'bar');
+});
